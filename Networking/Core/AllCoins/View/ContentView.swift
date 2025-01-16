825a7086b9197ccf1708ce714450b70f594ac8d0
aminebenjebli <80284883+aminebenjebli@users.noreply.github.com>
2025-01-16 22:19:31 +0100

    Lets start

---
+++ Networking/Core/AllCoins/View/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Networking
+//
+//  Created by AmineBj on 1/16/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack  {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, World!")
+            
+        }	
+        .padding()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
