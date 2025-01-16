0a0a376673e66c67306f16ac245190913787df59
aminebenjebli <80284883+aminebenjebli@users.noreply.github.com>
2025-01-16 22:00:37 +0100

    first commit

---
+++ Networking_Application/Networking_Application/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Networking_Application
+//
+//  Created by AmineBj on 1/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        Image(systemName: "globe")
+            .imageScale(.large)
+            .foregroundColor(.accentColor)
+        Text("Hello, World!")
+    }
+        .padding()
+}
+}
+
+struct ContentView_ProPreview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
