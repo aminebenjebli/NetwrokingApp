710b1852111dd45d28a326014543c02a4179c7fd
aminebenjebli <80284883+aminebenjebli@users.noreply.github.com>
2025-01-21 20:29:58 +0100

    handle(error-response)-MainThread
    MainThread and backgroundTread logic , handle error and response in the fetch , work with service to implement the fetch in within it and call the funciton in viewModel

---
+++ Networking/Core/AllCoins/Service/CoinDataService.swift
@@ -0,0 +1,58 @@
+//
+//  CoinDataService.swift
+//  Networking
+//
+//  Created by AmineBj on 1/21/25.
+//
+
+import Foundation
+public class CoinDataService {
+    
+    func fetchPrice(coin: String, completion: @escaping(Double) ->  Void) {
+        // Construct the URL string for the API request using the provided coin parameter.
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
+        
+        // Convert the string URL to a URL object. If it fails, exit the function.
+        guard let url = URL(string: urlString) else { return }
+        
+        
+        // Create a data task to fetch data from the specified URL.
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("DEBUG : Failed with error \(error.localizedDescription)")
+                //self.errorMessage = error.localizedDescription
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                //self.errorMessage = "BAD HTTP Response."
+                return
+            }
+            
+            guard httpResponse.statusCode == 200 else {
+                //self.errorMessage = "Failed to fetch with status code \(httpResponse.statusCode)"
+                return
+            }
+            
+            // Ensure that data is not nil; if it is, exit the function.
+            guard let data = data else { return }
+            
+            // Attempt to deserialize the received JSON data into a Swift dictionary.
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+            
+            
+            // Access the value associated with the coin key; expect it to be a dictionary of type [String: Double].
+            guard let value = jsonObject[coin] as? [String: Double] else {
+                print("Failed to parse value")
+                return
+            }
+            
+            // Retrieve the price in USD from the value dictionary. If it doesn't exist, exit the function.
+            guard let price = value["usd"] else { return }
+            // Update UI-related properties on the main thread since UI updates must occur on this thread.
+//                self.coin = coin.capitalized
+//                self.price = "$\(price)"
+            print("DEBUG: price in service is \(price)")
+            completion(price)
+        }.resume() // Start the data task.
+    }
+}
--- Networking/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ Networking/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -9,51 +9,21 @@ import Foundation
 class CoinsViewModel : ObservableObject {
     @Published var coin = ""
     @Published var price = ""
+    @Published var errorMessage: String?
+    
+    private let service = CoinDataService()
      
     init() {
-        fetchPrice(coin: "ethereum")
+        fetchPrice(coin: "coin")
     }
     
     func fetchPrice(coin: String) {
-        // Construct the URL string for the API request using the provided coin parameter.
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
-        
-        // Convert the string URL to a URL object. If it fails, exit the function.
-        guard let url = URL(string: urlString) else { return }
-        
-        print("fetching price..")
-        
-        // Create a data task to fetch data from the specified URL.
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            print("Did receive data \(data)")
-            
-            // Ensure that data is not nil; if it is, exit the function.
-            guard let data = data else { return }
-            
-            // Attempt to deserialize the received JSON data into a Swift dictionary.
-            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
-            
-            print(jsonObject)
-            
-            // Access the value associated with the coin key; expect it to be a dictionary of type [String: Double].
-            guard let value = jsonObject[coin] as? [String: Double] else {
-                print("Failed to parse value") // Log an error message if parsing fails.
-                return
-            }
-            
-            // Retrieve the price in USD from the value dictionary. If it doesn't exist, exit the function.
-            guard let price = value["usd"] else { return }
-            
-            // Update UI-related properties on the main thread since UI updates must occur on this thread.
+        service.fetchPrice(coin: coin) {priceFromService in
             DispatchQueue.main.async {
-                self.coin = coin.capitalized
-                self.price = "$\(price)"
+                self.price = "\(priceFromService)"
+                self.coin = coin
             }
             
-        }.resume() // Start the data task.
-        
-        print("Did reach end of function")
+        }
     }
-
 }
